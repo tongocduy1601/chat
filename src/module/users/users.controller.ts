b380fdd40b8063781e6252462fc5002d3f9da1ba
duyto <tongocduy1999@gmail.com>
2023-12-02 16:45:37 +0700

    init: authen

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { User, UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from 'src/database/entities/user.entity';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get('')
+    @UseGuards(AuthGuard)
     async findAll(): Promise<User[]> {
         return this.usersService.findOne('john');
     }
